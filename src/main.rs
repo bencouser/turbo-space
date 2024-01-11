a054c39c6006dfb884971bbee7ff09f640fb9ccf
bencouser <neb2010@live.co.uk>
2024-01-11 21:29:13 +0000

    created a small file tree

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let project_name = "project_name";
+
+    create_dir(&format!("{}/data/", project_name));
+    create_dir(&format!("{}/notebooks/", project_name));
+}
+
+fn create_dir(path: &str) {
+    match fs::create_dir_all(path) {
+        Ok(_) => println!("Created directory: {}", path),
+        Err(e) => println!("Error creating directory: {}", e),
+    }
 }
